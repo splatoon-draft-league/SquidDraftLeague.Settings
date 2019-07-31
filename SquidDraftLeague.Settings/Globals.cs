8ad650dc4c18de48db125fc89a46257d1bf273f4
Jordan Marine <zeotni.jordan@gmail.com>
2019-07-31 17:49:02 -0500

    Add project files.

---
+++ SquidDraftLeague.Settings/Globals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace SquidDraftLeague.Settings
+{
+    public static class Globals
+    {
+        public static List<ulong> SuperUsers => GetSuperUsers();
+
+        private static List<ulong> GetSuperUsers()
+        {
+            if (!File.Exists(Path.Combine(AppPath, "Data", "superusers.json")))
+            {
+                File.WriteAllText(Path.Combine(AppPath, "Data", "superusers.json"), JsonConvert.SerializeObject(new List<ulong>(), Formatting.Indented));
+            }
+
+            return JsonConvert.DeserializeObject<List<ulong>>(File.ReadAllText(Path.Combine(AppPath, "Data", "superusers.json")));
+        }
+
+        /// <summary>
+        /// Gets or sets the bots settings.
+        /// </summary>
+        public static Settings BotSettings { get; set; }
+
+        /// <summary>
+        /// Returns the root directory of the application.
+        /// </summary>
+        public static readonly string AppPath = Directory.GetParent(new Uri(Assembly.GetEntryAssembly()?.CodeBase).LocalPath).FullName;
+
+        /// <summary>
+        /// My implementation of a static random; as close to fully random as possible.
+        /// </summary>
+        public static Random Random => local ?? (local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId)));
+
+        [ThreadStatic] private static Random local;
+    }
+}
---
+++ SquidDraftLeague.Settings/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace SquidDraftLeague.Settings
+{
+    public class Settings
+    {
+        [JsonProperty("bot_token")]
+        public string BotToken { get; set; }
+
+        [JsonProperty("app_key")]
+        public string AppKey { get; set; }
+
+        [JsonProperty("base_id")]
+        public string BaseId { get; set; }
+
+        [JsonProperty("prefix")]
+        public string Prefix { get; set; }
+
+        public void SaveSettings()
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+
+            File.WriteAllText(Path.Combine(Globals.AppPath, "Data", "settings.json"), json);
+        }
+    }
+}
